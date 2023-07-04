7f56b049c19b25571222b81906537ddc3a71fcb3
ahmed marzoug <ahmed.merzoug@esprit.tn>
2023-07-04 18:20:03 +0200

    update setting for deployments

--- Program.cs
+++ Program.cs
@@ -45,11 +45,11 @@ builder.Services.AddDbContext<CityInfoContext>(
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
